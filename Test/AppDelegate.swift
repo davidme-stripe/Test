ed3ad1056edab947d911b8c72710ba967a1c9467
Yuki Tokuhiro <yuki@stripe.com>
2019-07-24 12:28:42 -0700

    Add sample project

--- Test/AppDelegate.swift
+++ Test/AppDelegate.swift
@@ -1,46 +1,59 @@
 //
 //  AppDelegate.swift
-//  Test
+//  Standard Integration
 //
-//  Created by Yuki Tokuhiro on 6/18/19.
-//  Copyright © 2019 Yuki Tokuhiro. All rights reserved.
+//  Created by Jack Flintermann on 1/15/15.
+//  Copyright (c) 2015 Stripe. All rights reserved.
 //
 
 import UIKit
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
+    
     var window: UIWindow?
 
-
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-//        window!.rootViewController = ViewController()
+        let rootVC = BrowseProductsViewController()
+        let navigationController = UINavigationController(rootViewController: rootVC)
+        let window = UIWindow(frame: UIScreen.main.bounds)
+        window.rootViewController = navigationController;
+        window.makeKeyAndVisible()
+        self.window = window
         return true
     }
-
-    func applicationWillResignActive(_ application: UIApplication) {
-        // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
-        // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
+    
+    // This method is where you handle URL opens if you are using a native scheme URLs (eg "yourexampleapp://")
+    func application(_ app: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey : Any] = [:]) -> Bool {
+        let stripeHandled = Stripe.handleURLCallback(with: url)
+        
+        if (stripeHandled) {
+            return true
+        }
+        else {
+            // This was not a stripe url, do whatever url handling your app
+            // normally does, if any.
+        }
+        
+        return false
     }
-
-    func applicationDidEnterBackground(_ application: UIApplication) {
-        // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
-        // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+    
+    // This method is where you handle URL opens if you are using univeral link URLs (eg "https://example.com/stripe_ios_callback")
+    func application(_ application: UIApplication, continue userActivity: NSUserActivity, restorationHandler: @escaping ([Any]?) -> Void) -> Bool {
+        if userActivity.activityType == NSUserActivityTypeBrowsingWeb {
+            if let url = userActivity.webpageURL {
+                let stripeHandled = Stripe.handleURLCallback(with: url)
+                
+                if (stripeHandled) {
+                    return true
+                }
+                else {
+                    // This was not a stripe url, do whatever url handling your app
+                    // normally does, if any.
+                }
+            }
+            
+        }
+        return false
     }
-
-    func applicationWillEnterForeground(_ application: UIApplication) {
-        // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
-    }
-
-    func applicationDidBecomeActive(_ application: UIApplication) {
-        // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
-    }
-
-    func applicationWillTerminate(_ application: UIApplication) {
-        // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
-    }
-
-
 }
-
